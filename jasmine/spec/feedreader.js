0a71834c283b353e64db6f4f471dca9b9f3cf309
Márcio Rodrigo <39975073+mrodrigochaves@users.noreply.github.com>
2018-07-08 18:17:48 -0300

    Update feedreader.js

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -52,22 +52,34 @@ $(function() {
 
 
     /* TODO: Write a new test suite named "The menu" */
-        describe('The menu', function(){
-
-        });
+       describe('theMenu', function(){
+            const theBody = $('body'),
+                  theMenuLink = $('.menu-icon-link');
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
          */
+           it('theMenuHidden', function(){
+            expect(theBody.hasClass('menu-hidden')).toBe(true);
+         });
 
          /* TODO: Write a test that ensures the menu changes
           * visibility when the menu icon is clicked. This test
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
+           it('theChangeMenu', function(){
+            theMenuLink.click();
+            expect(theBody.hasClass('menu-hidden')).not.toBe(true);
+            theMenuLink.click();
+            expect(theBody.hasClass('menu-hidden')).toBe(true);
+          });
+      });
 
     /* TODO: Write a new test suite named "Initial Entries" */
+        describe('initialEntries', function(){
+        });
 
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
